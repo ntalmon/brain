1eb13b7ee65d509bd6d5bb0ca6dbb1d67b9cbcd3
Noam Talmon <noamtalmon@mail.tau.ac.il>
2020-04-12 12:53:55 +0300

    Fixing parsers

--- brain/parsers/color_image.py
+++ brain/parsers/color_image.py
@@ -1,14 +1,16 @@
+import pathlib
+
 from PIL import Image
 
 
-def parse_color_image(data):
+def parse_color_image(data):  # TODO: after writing parsed file, delete raw file
     if 'color_image' not in data:
         return  # TODO: handle this case
     color_image = data['color_image']
     if 'width' not in color_image or 'height' not in color_image or 'path' not in color_image:
         return  # TODO: handle this case
     width, height, path = color_image['width'], color_image['height'], color_image['path']
-    new_path = ''  # TODO: find new path
+    new_path = str(pathlib.Path(path).parent / 'color_image.jpg')  # TODO: find new path
     with open(path, 'rb') as reader:
         data = reader.read()
     image = Image.frombytes('RGB', (width, height), data)
--- brain/parsers/depth_image.py
+++ brain/parsers/depth_image.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
+import pathlib
 
 
-def parse_depth_image(data):
+def parse_depth_image(data):  # TODO: after writing parsed file, delete raw file
     if 'depth_image' not in data:
         return  # TODO: handle this case
     depth_image = data['depth_image']
     if 'width' not in depth_image or 'height' not in depth_image or 'path' not in depth_image:
         return  # TODO: handle this case
     width, height, path = depth_image['width'], depth_image['height'], depth_image['path']
-    new_path = ''  # TODO: find new path
+    new_path = str(pathlib.Path(path).parent / 'depth_image.jpg')  # TODO: is it OK to save in the saver directory?
     array = np.load(path).reshape((width, height))
     plt.imshow(array)
     plt.savefig(new_path)
