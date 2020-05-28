13b717d03798d5b10e94899ab1e6583f749e2f09
Noam Talmon <noamtalmon@mail.tau.ac.il>
2020-05-28 12:03:50 +0300

    Trying to run build on setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
+import subprocess
+
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+def execute_command(command):
+    sp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    code, out, err = sp.returncode, sp.stdout.read(), sp.stderr.read()
+    return code, out, err
+
+
+class InstallCommand(install):
+    def run(self):
+        code, out, err = execute_command(['python', '-m', './scripts/build.sh', 'protobuf'])
+        if code != 0:
+            raise Exception(f'Failed to build protobuf: out={out}, err={err}')
+        install.run(self)
+
 
 setup(
     name='Brain',
     version='0.1.0',
     author='Noam Talmon',
+    cmdclass={'install': InstallCommand},
     description='Advanced System Design - Final Project',
     packages=find_packages(),
     install_requires=['click'],
